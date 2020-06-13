7da2bde445080a0532f0095b48f120c2f54996f2
Alex <agordon1123@gmail.com>
2020-06-12 22:07:44 -0400

    corrected offline weather null value

--- src/components/WeatherWidget.js
+++ src/components/WeatherWidget.js
@@ -15,19 +15,19 @@ const WeatherWidget = () => {
             axios
                 .get(`${process.env.REACT_APP_WEATHER_API_URL}/weather?zip=${user.zipcode},us&APPID=${process.env.REACT_APP_WEATHER_API_KEY}&units=imperial`)
                 .then(res => setWeather(res.data))
-                .catch(err => errorHandler(err.response, history));
+                .catch(err =>errorHandler(err.response, history));
         }
     }, [user.zipcode]);
 
     return (
         <Paper elevation={3} className='weather-widget-container'>
-            {Object.keys(weather).length && (
+            {Object.keys(weather).length ? (
                 <>
                     <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`} />
                     <h3>{weather.name}</h3>
                     <p>{Math.round(weather.main.temp)}</p>
                 </>
-            )}
+            ) : null}
         </Paper>
     );
 };
